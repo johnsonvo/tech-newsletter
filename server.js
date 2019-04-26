3dd082a3e25efaef8e754eb42e245326c5551845
johnsonvo <JOHNSONMVO@GMAIL.COM>
2019-04-25 18:41:31 -0700

    Added styled navbar

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const db = require('./models');
 //============================ MIDDLEWARE ============================//
 
 // Parse URL Encoded Data
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Parse JSON  Data
 app.use(bodyParser.json());
@@ -23,39 +23,45 @@ app.use(express.static(__dirname + '/public'));
 
 // ROOT Route
 app.get('/', (req, res) => {
-    res.send('<h1>Newsletter</h1>');
+	res.send('<h1>Newsletter</h1>');
 });
 
 // GET New User Route
 app.get('/signup', (req, res) => {
-    res.render('auth/signup');
+	res.render('auth/signup');
 });
 
 // POST Create User Route
 app.post('/signup', (req, res) => {
-    // console.log(req.body);
-    // res.send('<h1>Successsss</h1>');
-    db.User.create(newUser, (err, newUser) => {
-        if (err) return res.render('auth/signup', { errors: [err] });
-        res.redirect('/signup');
-    });
+	// console.log(req.body);
+	// res.send('<h1>Successsss</h1>');
 
+	// // Authentification
+	const errors = [];
+	// // take name as a string, .split with " ", returns array, check length of 2
+	// // if not, return error
 
-    // // Authentification
-    // const errors = [];
-    // // take name as a string, .split with " ", returns array, check length of 2
-    // // if not, return error
+	// req.body.name
 
-    // req.body.name
-
-
-})
+	db.User.create((err, newUser) => {
+		if (err) return res.render('auth/signup', { errors: [err] });
+		res.redirect('/signup');
+	});
+});
 
 // GET Dashboard
 app.get('/dashboard', (rew, res) => {
-    res.render('dashboard');
-})
+	res.render('dashboard');
+});
 
+//============================ API ROUTES ============================//
+// Helper route, just for development, so we can easily see the users in DB.
+app.get('/api/v1/users', (req, res) => {
+    db.User.find((err, allUsers) => {
+        if (err) res.json(err);
+        res.json(allUsers);
+    });
+});
 
 //============================ Start Server ============================//
 
